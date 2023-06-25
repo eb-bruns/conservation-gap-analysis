fe29ca75df87064d972b4f886809a8db36b9c3fb
eb-bruns <ebeckman@mortonarb.org>
2023-06-25 15:06:21 -0500

    small comment

--- spatial-analysis-workflow/1-prep_gis_layers.R
+++ spatial-analysis-workflow/1-prep_gis_layers.R
@@ -15,8 +15,6 @@
   ## None
 
 ## OUTPUTS
-  ## Ecoregions shapefile, either global, North American, or US (see 
-  #   descriptions within script below)
   ## world_countries_10m (folder with shapefile)
   #   World countries layer from Natural Earth (https://www.naturalearthdata.com), 
   #   downloaded using the rnaturalearth package and edited to remove lakes
@@ -24,6 +22,9 @@
   ## urban_areas_50m (folder with shapefile)
   #   Urban areas layer from Natural Earth:
   #   https://www.naturalearthdata.com/downloads/50m-cultural-vectors/50m-urban-areas/ 
+  ## (optional; only needed for script 8-calc_map_exsitu_coverage.R)
+  ## Ecoregions shapefile, either global, North American, or US (see 
+  #   descriptions within script below)
 
 ################################################################################
 # Load libraries
@@ -49,11 +50,60 @@ rm(my.packages)
 source("/Users/emily/Documents/GitHub/conservation-gap-analysis/spatial-analysis-workflow/0-set_working_directory.R")
 
 ################################################################################
+# Create layer for add country codes and clipping occurrence points to land
+################################################################################
+
+# we clip the occurrence points to the countries layer since some analyses rely
+#   on knowing which country (or sometimes state) the point falls within
+
+# download world countries layer using rnaturalearth package; vect converts to
+#   a terra object (SpatVector)
+world_ctry <- vect(ne_countries(scale = 10, type = "countries", 
+                                returnclass = "sf"))
+# download layer of lakes using rnaturalearth package
+lakes <- vect(ne_download(scale = 10, type = "lakes", category = "physical", 
+                          returnclass = "sf"))
+# remove lakes
+world_ctry <- erase(world_ctry,lakes)
+
+# there is an issue with the 10m countries dataset where some 2-digit country
+#   codes (which we need later) are -99; fixing...
+sort(unique(world_ctry$iso_a2)) # see if there is "-99" or NA.. if yes...
+# see which countries have the issue:
+unique(as.data.frame(world_ctry[which(
+  is.na(world_ctry$iso_a2) | world_ctry$iso_a2=="-99"),"name_en"])[,1])
+# looks like right now there are 22 "countries" with the issue, but most are
+#   islands or other small areas which we will ignore for now; if you see
+#   an area that is important to you, add it to the list below
+# fix the iso_a2 field
+world_ctry[which(world_ctry$admin=="Somaliland"),"iso_a2"] <- "SO"
+world_ctry[which(world_ctry$admin=="France"),"iso_a2"] <- "FR"
+world_ctry[which(world_ctry$admin=="Norway"),"iso_a2"] <- "NO"
+world_ctry[which(world_ctry$admin=="Kosovo"),"iso_a2"] <- "XK"
+
+# save file
+writeVector(world_ctry, file.path(main_dir,gis_dir,"world_countries_10m"),
+            filetype = "ESRI Shapefile")
+
+################################################################################
+# Download and save urban areas layer, in case you use for vetting points later
+################################################################################
+
+# download urban areas layer using rnaturalearth package
+urban <- vect(ne_download(scale = 50, type = "urban_areas", returnclass = "sf"))
+
+# save file
+writeVector(urban, file.path(main_dir,gis_dir,"urban_areas_50m"),
+            filetype = "ESRI Shapefile")
+
+################################################################################
+# ONLY USE THIS SECTION IF YOU PLAN TO USE SCRIPT 8-calc_map_exsitu_coverage.R
+#
 # **Manually** download ecoregions shapefile(s)
 ################################################################################
 
-# Depending on the scale/region of your analysis, choose which ecoregions you'd 
-#   like to use...
+# Depending on the scale/region of your analysis, choose one of the following 
+#   ecoregions to use in analyses and mapping...
 
   ## Global terrestrial ecoregions from The Nature Conservancy (48 MB)
   #   To download, go to 
@@ -77,50 +127,3 @@ source("/Users/emily/Documents/GitHub/conservation-gap-analysis/spatial-analysis
   #   The files will automatically download
   #   Place the downloaded folder (us_eco_l4_state_boundaries) in your 
   #   gis_layers folder
-
-################################################################################
-# Create layer for add country codes and clipping occurrence points to land
-################################################################################
-
-# we clip the occurrence points to the countries layer since some analyses rely
-#   on knowing which country (or sometimes state) the point falls within
-
-# download world countries layer using rnaturalearth package; vect converts to
-#   a terra object (SpatVector)
-world_ctry <- vect(ne_countries(scale = 10, type = "countries", 
-                                returnclass = "sf"))
-  # download layer of lakes using rnaturalearth package
-lakes <- vect(ne_download(scale = 10, type = "lakes", category = "physical", 
-                          returnclass = "sf"))
-  # remove lakes
-world_ctry <- erase(world_ctry,lakes)
-
-# there is an issue with the 10m countries dataset where some 2-digit country
-#   codes (which we need later) are -99; fixing...
-sort(unique(world_ctry$iso_a2)) # see if there is "-99" or NA.. if yes...
-  # see which countries have the issue:
-unique(as.data.frame(world_ctry[which(
-  is.na(world_ctry$iso_a2) | world_ctry$iso_a2=="-99"),"name_en"])[,1])
-  # looks like right now there are 22 "countries" with the issue, but most are
-  #   islands or other small areas which we will ignore for now; if you see
-  #   an area that is important to you, add it to the list below
-  # fix the iso_a2 field
-world_ctry[which(world_ctry$admin=="Somaliland"),"iso_a2"] <- "SO"
-world_ctry[which(world_ctry$admin=="France"),"iso_a2"] <- "FR"
-world_ctry[which(world_ctry$admin=="Norway"),"iso_a2"] <- "NO"
-world_ctry[which(world_ctry$admin=="Kosovo"),"iso_a2"] <- "XK"
-
-# save file
-writeVector(world_ctry, file.path(main_dir,gis_dir,"world_countries_10m"),
-            filetype = "ESRI Shapefile")
-
-################################################################################
-# Download and save urban areas layer, in case you use for vetting points later
-################################################################################
-
-# download urban areas layer using rnaturalearth package
-urban <- vect(ne_download(scale = 50, type = "urban_areas", returnclass = "sf"))
-
-# save file
-writeVector(urban, file.path(main_dir,gis_dir,"urban_areas_50m"),
-            filetype = "ESRI Shapefile")
